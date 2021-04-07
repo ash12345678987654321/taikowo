57dbcc38c4d6aa324e727ce2081560c48a3b1ac2
errorgorn <ashleykhoo2@gmail.com>
2021-04-07 08:38:28 +0800

    scraped faces

---
+++ get_faces.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import face_recognition
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib.patches as patches
+
+df=pd.read_csv('scraping youtube/metadata.csv',index_col=0)
+df=df.T
+
+def get_img(id):
+    return mpimg.imread('scraping youtube/thumbs/'+id+'.jpg')
+
+curr=0
+for id in df["id"]:
+    print(id,curr)
+    curr+=1
+    
+    img=get_img(id)
+    
+    locs=face_recognition.face_locations(img)
+    
+    for x in range(min(3,len(locs))):
+        t,r,b,l=locs[x]
+        
+        plt.imsave('faces/'+id+'_'+str(x)+'.jpg',img[t:b,l:r])
